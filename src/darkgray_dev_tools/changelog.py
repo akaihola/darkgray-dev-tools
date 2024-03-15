8eadafb2b7e772d278c06b3af9ed5d1510c99721
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-03-15 14:24:40 +0200

    Verbose string match error in `changelog.py`
    This helps troubleshooting if `CHANGES.rst` patching fails.

--- src/darkgray_dev_tools/changelog.py
+++ src/darkgray_dev_tools/changelog.py
@@ -17,7 +17,11 @@ def patch_changelog(next_version: Version, *, dry_run: bool) -> None:
     path = Path("CHANGES.rst")
     content = path.read_text(encoding="utf-8")
     before_unreleased = "These features will be included in the next release:\n\n"
-    insert_point = content.index(before_unreleased) + len(before_unreleased)
+    try:
+        insert_point = content.index(before_unreleased) + len(before_unreleased)
+    except ValueError as exc:
+        msg = f"{exc} {before_unreleased!r}"
+        raise ValueError(msg) from exc
     before = content[:insert_point]
     after = content[insert_point:]
     title = f"{next_version}_ - {datetime.now(tz=timezone.utc).date()}"
