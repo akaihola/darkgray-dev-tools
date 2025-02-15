1c2b5e17f251ce8ed7795b24278df686c4f2f79a
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-02-15 22:32:47 +0200

    ci: install all linting dependencies in a clean virtualenv

--- run-lint.sh
+++ run-lint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-uv sync --all-extras
+VIRTUAL_ENV=
+uv sync --quiet --all-extras --all-groups
+UV_PYTHON=.venv
 
 errors=0
 .venv/bin/pre-commit run --all-files graylint || errors=$?
